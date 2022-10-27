1f13d3fdbd1f6ea7425dd84025d421ed26195000
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-10-27 21:46:29 +0300

    Create plugin

---
+++ Source/TrickyFunctionLibrary/Private/TrickyUtilsLibrary.cpp
@@ -0,0 +1,5 @@
+// MIT License Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov
+
+
+#include "TrickyUtilsLibrary.h"
+
---
+++ Source/TrickyFunctionLibrary/Public/TrickyUtilsLibrary.h
@@ -0,0 +1,17 @@
+// MIT License Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "TrickyUtilsLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRICKYFUNCTIONLIBRARY_API UTrickyUtilsLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
